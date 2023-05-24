51ae2f0b83e465ced98d4c1952c0cdee65316574
rafid-dev <vorahsan@gmail.com>
2023-05-24 22:38:31 +0600

    Bench: 12196485

--- src/chess.cpp
+++ src/chess.cpp
@@ -216,7 +216,7 @@ void Board::makeMove(Move move) {
         const Piece rook = sideToMove == White ? WhiteRook : BlackRook;
         Square rookToSq  = file_rank_square(to_sq > from_sq ? FILE_F : FILE_D, square_rank(from_sq));
         Square kingToSq  = file_rank_square(to_sq > from_sq ? FILE_G : FILE_C, square_rank(from_sq));
-        if (((0x4 & from_sq) ^ (0x4 & kingToSq))) {
+        if (NNUE::KING_BUCKET[from_sq] != NNUE::KING_BUCKET[kingToSq]) {
             removePiece(p, from_sq);
             removePiece(rook, to_sq);
 
@@ -392,7 +392,7 @@ void Board::movePiece(Piece piece, Square fromSq, Square toSq, Square kSQ_White,
     board[fromSq] = None;
     board[toSq]   = piece;
 
-    if (type_of_piece(piece) == KING && ((0x4 & fromSq) ^ (0x4 & toSq))) {
+    if (type_of_piece(piece) == KING && NNUE::KING_BUCKET[fromSq] != NNUE::KING_BUCKET[toSq]) {
         refresh();
     } else {
         nnue->updateAccumulator(type_of_piece(piece), piece < BlackPawn ? White : Black, fromSq, toSq,
--- src/nnue.cpp
+++ src/nnue.cpp
@@ -5,7 +5,7 @@
 
 #define INCBIN_STYLE INCBIN_STYLE_CAMEL
 #include "incbin/incbin.h"
-INCBIN(EVAL, "./epoch_510.net");
+INCBIN(EVAL, "./epoch_580_bucket_net.net");
 
 using namespace Chess;
 
